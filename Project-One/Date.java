c233fb757b54f72429eb21660a47da03662d008d
Andrea Tongsak <andy.st2001@gmail.com>
2019-10-02 00:45:45 -0400

    Added toString, equals methods

--- Project-One/Date.java
+++ Project-One/Date.java
@@ -54,5 +54,55 @@ public class Date {
     return -1;
   }
   
+  // equals checks if two dates have the same day, month, and year
+  @Override 
+  public boolean equals(Object otherDate) {
+    Date compareDate = (Date)otherDate;
+    
+    if (this.getDay() == compareDate.getDay() && this.getMonth() == compareDate.getMonth() && this.getYear() == compareDate.getYear()) {
+      return true;
+    } else {
+      return false;
+    }
+    
+  }
+  
+  // toString converts date into an English representation 
+  @Override
+  public String toString() {
+    
+    // MONTHS
+    String printMonth = "";
+    if (this.getMonth() == 1) 
+      printMonth = "January";
+    else if (this.getMonth() == 2)
+      printMonth = "February";
+    else if (this.getMonth() == 3)
+      printMonth = "March";
+    else if (this.getMonth() == 4)
+      printMonth = "April";
+    else if (this.getMonth() == 5)
+      printMonth = "May";
+    else if (this.getMonth() == 6)
+      printMonth = "June";
+    else if (this.getMonth() == 7)
+      printMonth = "July";
+    else if (this.getMonth() == 8)
+      printMonth = "August";
+    else if (this.getMonth() == 9)
+      printMonth = "September";
+    else if (this.getMonth() == 10)
+      printMonth = "October";
+    else if (this.getMonth() == 11)
+      printMonth = "November";
+    else if (this.getMonth() == 12)
+      printMonth = "December";
+    else 
+      printMonth = "null";
+    
+    
+    return printMonth + " " + this.getDay() + ", " + this.getYear();
+  }
+  
   
 }
