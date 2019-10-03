2ac4c17f8548a8c6860c4685b5238efba3080e97
Andrea Tongsak <andy.st2001@gmail.com>
2019-10-02 20:45:20 -0400

    correct formatting applied

--- Project-One/LimitedInsurancePolicy.java
+++ Project-One/LimitedInsurancePolicy.java
@@ -16,15 +16,29 @@ public class LimitedInsurancePolicy extends InsurancePolicy {
   
   /* CONSTRUCTORS */
   
-//  public LimitedInsurancePolicy(String policyNumber, Date expirationDate, double copay, double deductible, 
-//                                double actuarialValue, boolean hasActualLimit, double annualLimit, 
-//                                boolean hasLifetimeLimit, double lifetimeLimit) {
-//    
-//  }
+  public LimitedInsurancePolicy(String policyNumber, Date expirationDate, double copay, double deductible, 
+                                double actuarialValue, boolean hasAnnualLimit, double annualLimit, 
+                                boolean hasLifetimeLimit, double lifetimeLimit) {
+    super(policyNumber, expirationDate, copay, deductible, actuarialValue);
+    this.hasAnnualLimit = hasAnnualLimit;
+    this.annualLimit = annualLimit;
+    this.hasLifetimeLimit = hasLifetimeLimit;
+    this.lifetimeLimit = lifetimeLimit;
+  }
+  
+  public LimitedInsurancePolicy(String policyNumber, Date expirationDate, double copay, double deductible, 
+                                double actuarialValue, boolean hasAnnualLimit, double annualLimit, 
+                                boolean hasOutOfPocketLimit, double outOfPocketLimit, boolean hasLifetimeLimit, 
+                                double lifetimeLimit, double expectedTenYearBenefit, double profitMargin, 
+                                InsurancePolicy supplementalInsurance) {
+    super(policyNumber, expirationDate, copay, deductible, actuarialValue, hasAnnualLimit, annualLimit, 
+          hasOutOfPocketLimit, outOfPocketLimit, hasLifetimeLimit, lifetimeLimit, expectedTenYearBenefit, profitMargin, 
+          supplementalInsurance);
+  }
   
   /* METHODS */
   
-  // A) setAnnualLimit sets whether policy has an annual limit, if so, what limit is
+  // A) sets whether policy has an annual limit, if so, what limit is
   public void setAnnualLimit(boolean hasAnnualLimit, double annualLimit) {
     this.hasAnnualLimit = hasAnnualLimit;
     if (this.hasAnnualLimit == true) {
@@ -32,12 +46,12 @@ public class LimitedInsurancePolicy extends InsurancePolicy {
     }
   }
   
-  // B) hasAnnualLimit returns if policy has annual limit (true) or not (false)
+  // B) returns if policy has annual limit (true) or not (false)
   public boolean hasAnnualLimit() {
     return hasAnnualLimit;
   }
   
-  // C) getAnnualLimit returns the annual limit for the policy. If policy has no annual limit, method returns non-positive value
+  // C) returns the annual limit for the policy. If policy has no annual limit, method returns non-positive value
   public double getAnnualLimit() {
     if (hasAnnualLimit == true) {
       return annualLimit;
@@ -46,7 +60,7 @@ public class LimitedInsurancePolicy extends InsurancePolicy {
     }
   }
   
-  // D) setLifetimeLimit returns whether policy has lifetime limit, if so, what it is
+  // D) returns whether policy has lifetime limit, if so, what it is
   public void setLifetimeLimit(boolean hasLifetimeLimit, double lifetimeLimit) {
     this.hasLifetimeLimit = hasLifetimeLimit;
     if (hasLifetimeLimit) {
@@ -54,12 +68,12 @@ public class LimitedInsurancePolicy extends InsurancePolicy {
     }
   }
   
-  // E) hasLifetimeLimit returns if policy has lifetime limit (true) or not (false)
+  // E) returns if policy has lifetime limit (true) or not (false)
   public boolean hasLifetimeLimit() {
     return hasLifetimeLimit;
   }
   
-  // F) getLifetimeLimit returns lifetime limit for the policy if there is one, else returns non-positive number
+  // F) returns lifetime limit for the policy if there is one, else returns non-positive number
   public double getLifetimeLimit() {
     if (hasLifetimeLimit && lifetimeLimit > 0) {
       return lifetimeLimit;
@@ -68,7 +82,7 @@ public class LimitedInsurancePolicy extends InsurancePolicy {
     }
   }
   
-  // G) applyYearlyLimit if policy has a yearly limit and (claim + yeartime benefit) exceeds yearly limit, amount is reduced by excess
+  // G) if policy has a yearly limit and (claim + yeartime benefit) exceeds yearly limit, amount is reduced by excess
   public double applyAnnualLimit(double claim) {
     if (hasAnnualLimit() == true && (claim + getYearlyBenefit()) > getAnnualLimit()) {
       return claim - ((claim + getYearlyBenefit()) - getAnnualLimit());
@@ -77,12 +91,32 @@ public class LimitedInsurancePolicy extends InsurancePolicy {
     }
   }
   
-  // H) applyLifetimeLimit has a lifetime limit and this checks to see if the claim exceeds it and returns the largest claim
+  // H) has a lifetime limit and this checks to see if the claim exceeds it and returns the largest claim
   public double applyLifetimeLimit(double claim) {
     if (hasLifetimeLimit() == true && (claim + getLifetimeBenefit()) > getLifetimeLimit()) {
       return claim - ((claim + getLifetimeBenefit()) - getLifetimeLimit());
     } else {
       return claim;
+    }
+  }
+  
+  // processClaim: after computing benefit and claim, method does other stuff
+  @Override 
+  public double processClaim(double claim, Date date) {
+    // must call super. to refer something from parent class
+    // use get 
+    return getYearlyBenefit();
+  }
+  
+  // checks if policy has lifetime limit and returns no more than the difference between 
+  // lifetime benefit and lifetime limit
+  @Override 
+  public double getExpectedTenYearBenefit() {
+    if (hasLifetimeLimit() == true) {
+      return getLifetimeBenefit() - getOutOfPocketLimit();
+    } else {
+      // check work
+      return 0.0;
     }
   }
   
