49424eca708570abbc1235d5604cb855369e7e44
Andrea Tongsak <andy.st2001@gmail.com>
2019-10-18 11:37:35 -0400

    Update HW2Tester.java

--- Project-Two/HW2Tester.java
+++ Project-Two/HW2Tester.java
@@ -15,21 +15,26 @@ public class HW2Tester {
   @Test
   public void testIsAlphabeticalOrder() {
     
-    assertTrue("Incorrectly claims \"ac!ffG1hz\" is not in alphabetical order", HW2.isAlphabeticalOrder("ac!ffG1hz"));
-    assertFalse("Incorrectly claims \"ac!nfG1h\" is in alphabetical order", HW2.isAlphabeticalOrder("ac!nfG1h"));
+    /* Test 0, test 1, test many */
     
-    // Test 0, test 1, test many
-    
-    // Test 0 (no letters)
-    assertTrue("Incorrectly claims \"11\" is not in alphabetical order", HW2.isAlphabeticalOrder("11"));
+    // Length
+    // Test 0 (length 0)
     assertTrue("Incorrectly claims \"\" is not in alphabetical order", HW2.isAlphabeticalOrder(""));
-    // Test 1 (one letter)
+    // Test 1 (length 1)
     assertTrue("Incorrectly claims \"a\" is not in alphabetical order", HW2.isAlphabeticalOrder("a"));
-    // Test many (many letters)
+    // Test many (length many)
     assertTrue("Incorrectly claims \"abcd\" is not in alphabetical order", HW2.isAlphabeticalOrder("abcd"));
     
+    // Letters
+    // Test 0 (no letters)
+    assertTrue("Incorrectly claims \"11\" is not in alphabetical order", HW2.isAlphabeticalOrder("11"));
+    // Test 1 (1 letters)
+    assertTrue("Incorrectly claims \"11\" is not in alphabetical order", HW2.isAlphabeticalOrder("a2"));
+    // Test many (many letters)
+    assertTrue("Incorrectly claims \"11\" is not in alphabetical order", HW2.isAlphabeticalOrder("a2bdeh"));
     
-    // Test first, test middle, test last
+    
+    /* Test first, test middle, test last */
     
     // Test first (occurance of no order in beginning of string)
     assertFalse("Incorrectly claims \"zac!ffG1hz\" is in alphabetical order", HW2.isAlphabeticalOrder("zac!ffG1hz"));
@@ -61,27 +66,39 @@ public class HW2Tester {
     String expectedStr5 = "Heo there!";
     String expectedStr6 = "Hello there";
                  
-    // Test 0, test 1, test many
+    /* Test 0, test 1, test many */
     
-    // Test 0 (no char needs to be removed)
-    assertEquals(str, HW2.removeNchars(str, 2, 'j'));
-    
-    // Test 1 (one char must be removed)
-    assertEquals(expectedStr3, HW2.removeNchars(str, 1, 'h'));
-    
-    // Test many (many chars must be removed)
-    assertEquals(expectedStr1, HW2.removeNchars(str, 2, 'e'));
+    // Length
+    // Test 0 (length 0)
+    assertEquals("", HW2.removeNchars("", 2, 'j'));
+    // Test 1 (length 1)
+    assertEquals("", HW2.removeNchars("j", 1, 'j'));
+    // Test Many (length many!)
     assertEquals(expectedStr2, HW2.removeNchars(str, 10, 'e'));
     
+    // Integers
+    // Test 0 (remove 0)
+    assertEquals(str, HW2.removeNchars(str, 0, 'j'));
+    // Test 1 (remove 1)
+    assertEquals(expectedStr4, HW2.removeNchars(str, 1, 'H'));
+    // Test Many (remove many!)
+    assertEquals(expectedStr2, HW2.removeNchars(str, 10, 'e'));
     
-    // Test first, test middle, test last
+    // Letters
+    // Test 0 (no char)
+    assertEquals("00", HW2.removeNchars("00", 1, 'j'));
+    // Test 1 (1 char)
+    assertEquals("a", HW2.removeNchars("ab", 1, 'b'));
+    // Test many (many char)
+    assertEquals("a", HW2.removeNchars("abbbb", 4, 'b'));
+    
+    
+    /* Test first, test middle, test last */
     
     // Test first (first char remove)
     assertEquals(expectedStr4, HW2.removeNchars(str, 1, 'H'));
-    
     // Test middle (middle char remove)
     assertEquals(expectedStr5, HW2.removeNchars(str, 3, 'l'));
-    
     // Test last (last char remove)
     assertEquals(expectedStr6, HW2.removeNchars(str, 3, '!'));
   }
@@ -97,25 +114,37 @@ public class HW2Tester {
   @Test
   public void testRemoveString() {
     
-    // Test first
-    assertEquals("lle", HW2.removeString("ellelle", "elle"));
+    /* Test 0, test 1, test many */
     
-    // Test middle
-    assertEquals("elle", HW2.removeString("elellele", "elle"));
+    // Length
+    // Test 0 (length 0)
+    assertEquals("", HW2.removeString("", "elle"));
+    assertEquals("elle", HW2.removeString("elle", ""));
+    // Test 1 (length 1)
+    assertEquals("e", HW2.removeString("e", "elle"));
+    assertEquals("ll", HW2.removeString("elle", "e"));
+    // Test Many (length many!)
+    assertEquals("e", HW2.removeString("elleeelle", "elle"));
     
-    // Test last
-    assertEquals("elel", HW2.removeString("elelelle", "elle"));
-    
-    
+    // Word
     // Test 0 (no word)
     assertEquals("ellg", HW2.removeString("ellg", "elle"));
     assertEquals("elelgele", HW2.removeString("elelgele", "elle"));
-    
     // Test 1 (one instance of word)
     assertEquals("", HW2.removeString("elle", "elle"));
-    
     // Test many (two instances of word)
     assertEquals("ll", HW2.removeString("ellellelle", "elle"));
+    
+    
+    /* Test first, test middle, test last */
+    
+    // Test first
+    assertEquals("lle", HW2.removeString("ellelle", "elle"));
+    // Test middle
+    assertEquals("elle", HW2.removeString("elellele", "elle"));
+    // Test last
+    assertEquals("elel", HW2.removeString("elelelle", "elle"));
+    
     
   }
   
@@ -130,23 +159,31 @@ public class HW2Tester {
     // Should return "abcdXefXXXghiXjXXXX"
     // HW2.moveAllXsRight('X', "abcXdeXXXfghXiXXjXX") 
     
+    /* Test 0, test 1, test many */
+    
+    // Length
+    // Test 0 (length 0)
+    assertEquals("", HW2.moveAllXsRight('X', ""));
+    // Test 1 (length 1)
+    assertEquals("X", HW2.moveAllXsRight('X', "X"));
+    // Test many (length many)
+    assertEquals("jXuju", HW2.moveAllXsRight('X', "Xjuju"));
+
+    // Char
+    // Test 0 (no char move)
+    assertEquals("ab", HW2.moveAllXsRight('X', "ab"));
     // Test 1 (one char move right)
     assertEquals("abXcd", HW2.moveAllXsRight('X', "aXbcd"));
     assertEquals("abXcdefXg", HW2.moveAllXsRight('X', "aXbcdeXfg"));
-    
-    // Test 0 (no char move)
-    assertEquals("ab", HW2.moveAllXsRight('X', "ab"));
-    
     // Test many (multiple chars move)
     assertEquals("abcdXefXXXghiXjXXXX", HW2.moveAllXsRight('X', "abcXdeXXXfghXiXXjXX"));
     
     
+    /* Test first, test middle, test last */
     // Test first (first index)
     assertEquals("aXb", HW2.moveAllXsRight('X', "Xab"));
-    
     // Test middle (middle of String)
     assertEquals("abcdeXXfg", HW2.moveAllXsRight('X', "abcdXXefg"));
-    
     // Test last (end of String)
     assertEquals("aX", HW2.moveAllXsRight('X', "aX"));
     
@@ -184,7 +221,6 @@ public class HW2Tester {
 //    assertArrayEquals(expectRow2, HW2.table[2]);
 //    assertArrayEquals(expectRow3, HW2.table[3]);
     
-    //assertTrue(Arrays.deepEquals(expectedBoard, HW2.table));
   }
   
   /**
@@ -194,6 +230,7 @@ public class HW2Tester {
    * and it should slide that character in the array down and to the left as far as it can go. 
    * Any characters on that diagonal are slide up and to the right to fill.
    */
+  
   
   
   
