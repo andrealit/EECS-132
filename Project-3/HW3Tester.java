eb5a716dcd778ac3ee3a187c4a66f8799b617dfd
Andrea Tongsak <atongsak@gmail.com>
2019-10-31 14:04:17 -0400

    Update HW3Tester.java

--- Project-3/HW3Tester.java
+++ Project-3/HW3Tester.java
@@ -8,17 +8,27 @@ import static org.junit.Assert.*;
 
 /* 
  * A testing class for HW2 Methods 1-4
- * 
+ * A conditional statement will need tests that go through each branch of the execution. 
+ * Any loops will need tests that cover "test 0, test 1, test many" and "test first, test middle, test last"
  */ 
 public class HW3Tester {
   /**
-   * Test the update method.
-   * method update must be tested with string inputs of different
+   * Test the update and lookup method in State class.
    */
   @Test
-  public void testUpdate() {
+  public void testState() {
     
+    State stateTest = new State();
+
+    stateTest.update("x", 10);
+    assertEquals(10, stateTest.lookup("x"));
+
   }
+  
+  /*
+   * Test the 
+   */ 
+  
   
   
   
